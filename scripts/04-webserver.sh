34b5cca300776b5a1e51b0ec6451411f5ba97a17
mtsbatalha <mtsbatalha@gmail.com>
2025-12-29 03:18:38 -0300

    Remove pagespeed directive from Nginx config

--- scripts/04-webserver.sh
+++ scripts/04-webserver.sh
@@ -202,9 +202,6 @@ server {
     gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
     gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/wasm application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
 
-    # Pagespeed disabled
-    pagespeed off;
-
     # HTTP response headers for static content
     location = /robots.txt {
         allow all;
